c4c3048b90665892fa9b312e49cdd59126ac91a8
Frederick-Rive <270121618@yoobeestudent.ac.nz>
2025-11-16 17:47:44 +1300

    Market functions, debugging framework

--- Assets/Scripts/PortScript.cs
+++ Assets/Scripts/PortScript.cs
@@ -7,15 +7,18 @@ public class PortScript : MonoBehaviour
 {
 
     public Market market;
+    public GameObject debugText;  
      
     // Start is called before the first frame update
     void Start()
     {
         market = new Market("Test", 100);
 
-        market.AddGood(new Good("Goods", 10.0f, 3, GoodsCategory.Treasures));
+        market.AddGood(new MarketGood("Wheat", 3.0f, 3, GoodsCategory.Treasures, 1));
+        market.AddGood(new MarketGood("Wood", 4.0f, 3, GoodsCategory.Treasures, 1));
+        market.AddGood(new MarketGood("Gemstones", 10.0f, 3, GoodsCategory.Treasures, 1));
 
-        Debug.Log(market.GetGood(0).name);
+        Debug.Log(market.DebugPrintState());
     }
 
     // Update is called once per frame
